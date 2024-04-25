2b71433b4654937719b326771cb4549b7b8e78f4
Vishnu1173 <vishnurandy1173@gmail.com>
2024-04-25 12:56:48 +0530

    CalcBasics

---
+++ Calc/Bill.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Calc
+{
+    /// <summary>
+    /// Interaction logic for Bill.xaml
+    /// </summary>
+    public partial class Bill : Window
+    {
+        PatientPage patientPage;
+        EligiblePage eligiblePage;
+        ClaimPage claimPage;
+        public Bill()
+        {
+            InitializeComponent();
+            patientPage=new PatientPage();
+            eligiblePage=new EligiblePage();
+            claimPage=new ClaimPage();
+            framemain.Content=patientPage;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            framemain.Content = patientPage;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            framemain.Content = eligiblePage;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            framemain.Content = claimPage;
+        }
+
+        private void btnlog_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
---
+++ Calc/Menu1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Calc
+{
+    /// <summary>
+    /// Interaction logic for Menu1.xaml
+    /// </summary>
+    public partial class Menu1 : Window
+    {
+        PatientPage patientPage;
+        EligiblePage eligiblePage;
+        ClaimPage claimPage;
+        public Menu1()
+        {
+            InitializeComponent();
+            patientPage = new PatientPage();
+            eligiblePage = new EligiblePage();
+            claimPage = new ClaimPage();
+            framemain.Content = patientPage;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            framemain.Content = patientPage;
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            framemain.Content = eligiblePage;
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            framemain.Content = claimPage;
+        }
+    }
+}
